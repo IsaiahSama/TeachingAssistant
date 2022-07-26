18897361557f1728b65311070ad3603df2390305
Isaiah-Sama <zeldaplayergl@gmail.com>
2022-07-26 17:38:02 -0400

    Added command functionality

--- TypeForMe/Typeinfo.py
+++ TypeForMe/Typeinfo.py
@@ -1,7 +1,7 @@
 """Script used to take control of the keyboard and type output to the screen"""
-
-from xml.dom.minidom import TypeInfo
 import keyboard
+
+from time import sleep
 from tkinter import Tk, filedialog
 
 def Print(*message):
@@ -20,7 +20,20 @@ class TypeInfoException(Exception):
         super().__init__(message)
 
 class Typer:
-    """Class used to manage the typing of scripts and the like"""
+    """Class used to manage the typing of scripts and the like.
+    
+    Attrs:
+        script_text (list): The lines of the script
+        commands (list): A list of commands that can be used to control the Typer
+    
+    Methods:
+        get_file -> str: used to get the file containing the script to be typed
+        load_script -> None: used to load the script to be typed from the file
+        is_command -> bool: Checks if the current line contains a command for the Typer
+        exe_command -> None: Executes a command for the Typer
+        type_it -> None: Used to type the script line by line to the current active window.
+        
+    """
 
     def __init__(self) -> None:
         self.script_text = None
@@ -48,6 +61,45 @@ class Typer:
 
         if not self.script_text:
             raise TypeInfoException("Script file was empty!")
+
+    def is_command(self, msg:str) -> bool:
+        """Used to detect whether a given line of text is a command or not
+        
+        Args:
+            msg (str): The message to check."""
+        
+        text = msg.split(" ")
+        for cmd in self.commands:
+            if text[0] == cmd:
+                self.cmd = cmd
+                return True
+        return None
+
+    def exe_command(self, msg:str) -> bool:
+        """Used to execute a given command.
+        
+        Args:
+            msg (str): The string containing the command.
+        
+        Returns:
+            Bool"""
+        
+        # Parsing time.
+        if self.cmd == "wait":
+            text = msg.split("wait")[1:]
+            val = text[-1].strip()
+            if not val.isnumeric():
+                Print(val, "is not a number, so no waiting will be done.")
+                return False
+            secs = float(val)
+            Print("Waiting", secs, "seconds")
+            sleep(secs)
+
+    def type_it(self):
+        """Method responsible for actually typing to the current window."""
+        
+        Print("Starting in 5 seconds")
+        sleep(5)
 
 if __name__ == "__main__":
     main = Typer()
