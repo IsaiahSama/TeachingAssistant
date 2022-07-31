efdf98d0215986a582cc67419449d57b8075d189
Isaiah-Sama <zeldaplayergl@gmail.com>
2022-07-31 00:12:53 -0400

    Female voice will now use Google TTS instead of pyttsx3

--- Engine.py
+++ Engine.py
@@ -1,6 +1,10 @@
 """File responsible for handling the text to speech engine"""
 
 import pyttsx3
+import pyglet
+from gtts import gTTS
+from os import path, remove
+from time import sleep
 
 FEMALE_VOICE = 1
 MALE_VOICE = 0
@@ -12,16 +16,19 @@ class Engine:
         messages (list): List of messages to be read
         rate (int): The speaking rate of the tts
         voice (int): The type of voice for the tts. 0 for MALE_VOICE, 1 for FEMALE_VOICE
+        filename (str): The name of the temporary file
     Methods:
         setup_engine(): Used to setup the Text To Speech.
         say_and_print(*args): Prints a message to the screen, and says it as well.
-        speak(message:str): Stores messages to be output in a queue."""
+        speak(message:str): Stores messages to be output in a queue.
+        speak_female_voice(message:str): Uses gTTS to speak in a female voice."""
 
     messages = []
 
     def __init__(self) -> None:
         self.rate = 120
         self.voice = MALE_VOICE
+        self.filename = "output.mp3"
 
     def setup_engine(self):
         """Sets up the pyttsx3 engine for usage.
@@ -45,13 +52,28 @@ class Engine:
         print("ENGINE.PY:", message)
         self.speak(message)
 
+    def speak_female_voice(self, message:str):
+        """Used to speak using the female voice from gTTs instead of from Pyttsx3
+        
+        Args:
+            message (str): The message to be read"""
+        
+        tts = gTTS(message, lang='en')
+        tts.save(self.filename)
+        speech = pyglet.media.load(self.filename, streaming=False)
+        speech.play()
+
+        sleep(speech.duration)
+        if path.exists(self.filename): remove(self.filename)
+
     def speak(self, message:str):
         """Reads a message from the queue
         
         Args:
             message (str): The message to be read."""
-
-        engine = self.setup_engine()
-        engine.say(message)
-        engine.runAndWait()
-        engine.stop()
+        if self.voice == FEMALE_VOICE: self.speak_female_voice(message)
+        else:
+            engine = self.setup_engine()
+            engine.say(message)
+            engine.runAndWait()
+            engine.stop()
